c5d63eb608e8447adc158f47a91594189da38380
Blake Hagen <blakehagen@gmail.com>
2015-10-23 15:10:23 -0600

    tweet actions hide and show

--- js/animations.js
+++ js/animations.js
@@ -2,9 +2,11 @@ $(document).ready(function(){
 	
 	$('#tweet-controls').hide();
 	
+	$('.tweet-actions').hide();
+	
 	$('.tweet-compose').on('click', function(){
 		$('#tweet-controls').show();
-		$(this).css('height', '5em');
+		$('.tweet-compose').css('height', '5em');
 	});
 	
 	$('.tweet-compose').on('keyup keydown keypress', function(){
@@ -25,11 +27,29 @@ $(document).ready(function(){
 		}
 	});
 	
-	
 	$('#tweet-submit').on('click', function(){
-		$('#stream').prepend('<div class="tweet"><div class="content"><img class="avatar" src="img/alagoon.jpg" /><strong class="fullname">Blake H</strong><span class="username">@blakeh</span><p class="tweet-text">' + $('.tweet-compose').val() + '</p><div class="tweet-actions"><ul><li><span class="icon action-reply"></span> Reply</li><li><span class="icon action-retweet"></span> Retweet</li><li><span class="icon action-favorite"></span> Favorite</li><li><span class="icon action-more"></span> More</li></ul></div><div class="stats"><div class="retweets"><p class="num-retweets">30</p><p>RETWEETS</p></div><div class="favorites"><p class="num-favorites">6</p><p>FAVORITES</p></div><div class="users-interact"><div><img src="img/vklimenko.jpg" /></div></div><div class="time">1:04 PM - 19 Sep 13</div></div></div></div>');
-			
+		
+		$('#stream').prepend('<div class="tweet"><div class="content"><img class="avatar" src="img/alagoon.jpg" /><strong class="fullname">Blake H</strong><span class="username"> @blakeh</span><p class="tweet-text">' + $('.tweet-compose').val() + '</p><div class="stats"><div class="retweets"><p class="num-retweets">30</p><p>RETWEETS</p></div><div class="favorites"><p class="num-favorites">6</p><p>FAVORITES</p></div><div class="users-interact"><div><img src="img/vklimenko.jpg" /></div></div><div class="time">1:04 PM - 19 Sep 13</div></div></div></div>');
+		
+		$('.tweet-compose').val('');
+		$('#tweet-controls').hide();
+		$('.tweet-compose').css('height', '2.5em');
+		var remaining = 140;
+		var count = $('.tweet-compose').val().length;
+		$('#char-count').text(remaining);
+		
 	});
+	
+	$('.tweet').on('mouseenter', function() {
+		$('.tweet-actions',this).show();
+	});
+	
+	$('.tweet').on('mouseleave', function() {
+		$('.tweet-actions', this).hide();
+	});
+
+	
+	
 	
 	
 	
